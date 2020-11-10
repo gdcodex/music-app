638072980b05dadc993bc19493b9cc9339271288
Geremsa <geremsa111@gmail.com>
2020-11-10 16:45:02 +0530

    env file fixed

--- src/endpoints/searchapi.js
+++ src/endpoints/searchapi.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
-const REACT_APP_API_KEY = "AIzaSyDKuWFzMBrW56RNuKx2LKSxRrmhozw-agY AIzaSyAYZXRLgSOyrCP9ZOQYWGj4KjiaiGMjdu4 AIzaSyCLojsm8QEwZ7dpw-QIR2vkGyH0I2vNcvo AIzaSyANDlzLdOSuFpgPTznj-dDvsSUiCRMp43w AIzaSyCTbD8irYLboVu2Rvi9wFgzVxKhahcmn1E AIzaSyAGW_xTf2CEZN2uHt1M-2YJyQqna_fxATA AIzaSyDF1fohAW3Pu0UDbvZ7o6IGRQnSIIKDsK0"
+
 const Random =() =>{
-    const value = REACT_APP_API_KEY.split(" ");
+    const value = process.env.REACT_APP_API.split(" ");
     const key = Math.floor(Math.random()* Math.floor(value.length));
     return value[key];
 }
